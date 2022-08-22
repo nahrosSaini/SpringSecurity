233fc7c56bb1c894984078b4122d5f3a8c258c23
nahrosSaini <sainiroshan2020@gmail.com>
2022-08-22 16:19:29 +0530

    Added the csrf support for login page and logout button

--- Database Authentication/src/main/java/com/roshan/database_authentication/security/SecurityConfiguration.java
+++ Database Authentication/src/main/java/com/roshan/database_authentication/security/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().hasRole("USER").and()
