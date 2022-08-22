bfa9b1e5ecda2420b49f38b3fc83f10e0dd2a1d8
nahrosSaini <sainiroshan2020@gmail.com>
2022-08-22 16:28:30 +0530

    @AuthenticationPrincipal - passing the loggedIn user to controller and to view

--- Database Authentication/src/main/java/com/roshan/database_authentication/controllers/LoginController.java
+++ Database Authentication/src/main/java/com/roshan/database_authentication/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package com.roshan.database_authentication.controllers;
 
+import com.roshan.database_authentication.models.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +15,9 @@ public class LoginController {
     }
 
     @GetMapping("/dashboard")
-    public String getDashboard(){
+    public String getDashboard(@AuthenticationPrincipal User user, ModelMap modelMap){
+
+        modelMap.put("user",user);
         return "dashboard";
     }
 }
