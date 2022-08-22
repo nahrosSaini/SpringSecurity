7cd802af7f895d74f5756699a5f11cf7e6bd4535
nahrosSaini <sainiroshan2020@gmail.com>
2022-08-22 15:37:33 +0530

    Databased Authentication

---
+++ Database Authentication/Database Authentication/src/main/java/com/roshan/database_authentication/controllers/Controllers.java
@@ -0,0 +1,13 @@
+package com.roshan.database_authentication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controllers {
+
+    @GetMapping(value = "/")
+    public String getDefault(){
+        return "dashboard";
+    }
+}
---
+++ Database Authentication/Database Authentication/src/main/java/com/roshan/database_authentication/models/User.java
@@ -0,0 +1,50 @@
+package com.roshan.database_authentication.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy = "user")
+    private Set<Authorities> authorities=new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+}
